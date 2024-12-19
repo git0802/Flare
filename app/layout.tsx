df75f196868fda31aadf1eacd9c9fa0d622356f9
git0802 <lovebee0802@gmail.com>
2024-12-19 02:38:24 -0500

    feat(layout): integrate MainNav and Toaster components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { MainNav } from "@/components/main-nav";
+import { Toaster } from "@/components/ui/toaster";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <MainNav />
+        <main className="flex-1 p-10">{children}</main>
+        <Toaster />
       </body>
     </html>
   );
